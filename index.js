42d889b336da6011664dc7a98a09bb482d9c9c20
Istvan <szeles0619@gmail.com>
2023-03-17 13:24:55 +0000

    HTML structure added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -10,6 +11,162 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
+let newEmployeesArr = [];
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+const generateManager = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: "What's the manager's name? ",
+            name: 'name'
+        },
+        {
+            type: 'input',
+            message: "What's the manager's ID number? ",
+            name: 'id'
+        },
+        {
+            type: 'input',
+            message: "What's the manager's email address? ",
+            name: 'email'
+        },
+        {
+            type: 'input',
+            message: "What's the manager's office number? ",
+            name: 'office'
+        }
+    ])
+        .then((info) => {
+            const { name, id, email, office } = info;
+
+            const manager = new Manager(name, id, email, office);
+            newEmployeesArr.push(manager);
+            teamEditMenu();
+        }
+        )
+}
+
+// functions to get engineer info and push the engineer object to employee array
+const generateEngineer = (info) => {
+    const { name, id, email, github } = info;
+    const engineer = new Engineer(name, id, email, github);
+    employeesArr.push(engineer);
+}
+
+const getEngineerInfo = () => {
+    inquirer.prompt([
+            {
+                type: 'input',
+                message: "What's the engineer's name? ",
+                name: 'name',
+            },
+            {
+                type: 'input',
+                message: "What's the engineer's ID number? ",
+                name: 'id'
+            },
+            {
+                type: 'input',
+                message: "What's the engineer's email address? ",
+                name: 'email'
+            },
+            {
+                type: 'input',
+                message: "What's the engineer's github username? ",
+                name: 'github'
+            }
+        ])
+        .then((info) => {
+            generateEngineer(info);
+            teamEditMenu();
+        })
+}
+
+// functions to get Intern info and push the Intern object to employee array
+const generateIntern = (info) => {
+    const { name, id, email, school } = info;
+    const intern = new Intern(name, id, email, school);
+    employeesArr.push(intern);
+}
+
+const getInternInfo = () => {
+    inquirer.prompt([
+            {
+                type: 'input',
+                message: "What's the intern's name? ",
+                name: 'name'
+            },
+            {
+                type: 'input',
+                message: "What's the intern's ID number: ",
+                name: 'id'
+            },
+            {
+                type: 'input',
+                message: "What's the intern's email address? ",
+                name: 'email'
+            },
+            {
+                type: 'input',
+                message: "What's the intern's school? ",
+                name: 'school'
+            }
+        ])
+        .then((info) => {
+            generateIntern(info);
+            teamEditMenu();
+        })
+}
+
+const teamEditMenu = () => {
+    inquirer.
+    prompt([
+    {
+        type: 'list',
+        message:'What do you want to do next?',
+        name: 'choice',
+        choices: ['Add an engineer', 'Add an intern', 'Finish building the team']
+    }
+    ])
+    .then((response) => {
+        if (response.choice === 'Add an engineer') {
+            getEngineerInfo();
+        } else if (response.choice === 'Add an intern') {
+            getInternInfo();
+        } else {
+            console.log('Finish team build');
+            const teamInfo = render(newEmployeesArr);
+            completeTeam(teamInfo);
+        }
+    })
+}
+
+const completeTeam = (teamInfo) => {
+    // create the output folder if the folder doesn't exist
+    if(!fs.existsSync(OUTPUT_DIR)) {
+        fs.mkdir((OUTPUT_DIR), (err) => {
+            if (err) {
+                return console.error(err);
+            }
+        })
+    }
+    // write the data to the html file
+    fs.writeFile(outputPath, teamInfo, (err) => {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log('The team page has been created.');
+        }
+    })
+}
+/*---------------------------------------------------------*/
+
+/*----------------Application initialization---------------*/
+const init = () => {
+    generateManager();
+}
+
+init();
+/*---------------------------------------------------------*/
